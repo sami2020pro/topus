097dd905d3bbdf9a61bbd346542f973bd6bc8a6b
sami2020pro <samprogram2007@gmail.com>
2021-03-01 00:11:08 +0330

    update: remove and add some code to the vm.go

--- src/topus/vm/vm.go
+++ src/topus/vm/vm.go
@@ -1,21 +1,5 @@
 package vm
 
-var ()
-
-type Opcode struct {
-	instruction byte
-}
-
-func NewOpcode(instruction byte) *Opcode {
-	o := &Opcode{}
-	o.instruction = instruction
-	return o
-}
-
-func (o *Opcode) String() string {
-
-}
-
-func (o *Opcode) Value() byte {
-	return (o.instruction)
+func Run() {
+	
 }
